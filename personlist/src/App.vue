b8dcc33d42567ff8683aeddf5980068e81ef21b4
BraxindorUiA <espels16@uia.no>
2022-05-16 07:26:11 +0200

    Ble lite dette her.

---
+++ personlist/src/App.vue
@@ -0,0 +1,72 @@
+<script setup>
+import Header from "./components/Header.vue";
+</script>
+
+<template>
+  <div class="container">
+  <Header/>
+  </div>
+  <main>
+
+  </main>
+</template>
+
+<style>
+@import './assets/base.css';
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}*/
+
+header {
+  line-height: 1.5;
+}
+
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+
+/*a,
+.green {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+}*/
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+
+@media (min-width: 1024px) {
+  body {
+    display: flex;
+    place-items: center;
+  }
+
+  header {
+    display: flex;
+    place-items: center;
+    align-self: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+/*  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }*/
+
+}
+</style>
---
+++ personlist/src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <h1>Hello Otrum</h1>
+  </header>
+</template>
+
+<script>
+import Button from "./Button.vue";
+export default {
+  name: 'Header',
+  components: {Button},
+}
+</script>
+<style>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+  align-content: space-evenly;
+}
+</style>
